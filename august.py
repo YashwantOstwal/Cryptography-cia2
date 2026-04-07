27e7aabd90d8e281cce642ee1627edf347e26114
Yashwant Ostwal <140978232+YashwantOstwal@users.noreply.github.com>
2026-04-07 16:19:05 +0530

    updates

--- august.py
+++ august.py
@@ -1,31 +1,36 @@
-def august_cipher_encrypt(text):
-    encrypted_message = ""
-    for char in text:
-        if char.isalpha():
-            start = ord('A') if char.isupper() else ord('a')
-            shifted = (ord(char) - start + 1) % 26 + start
-            encrypted_message += chr(shifted)
+dict1 = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4,
+         'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
+         'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14,
+         'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+         'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25,
+         '1': 26, '2': 27, '3': 28, '4': 29, '5': 30, '6': 31}
+
+dict2 = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E',
+         5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
+         10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O',
+         15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T',
+         20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z',
+         26: '1', 27: '2', 28: '3', 29: '4', 30: '5', 31: '6'}
+
+def xor_august_cipher(text):
+    result_text = ''
+    key = 1 
+    
+    for letter in text.upper():
+        if letter in dict1:
+            x = dict1[letter] ^ key
+            result_text += dict2[x]
         else:
-            encrypted_message += char
-    return encrypted_message
+            result_text += letter
+            
+    return result_text
 
-def august_cipher_decrypt(text):
-    decrypted_message = ""
-    for char in text:
-        if char.isalpha():
-            start = ord('A') if char.isupper() else ord('a')
-            shifted = (ord(char) - start - 1) % 26 + start
-            decrypted_message += chr(shifted)
-        else:
-            decrypted_message += char
-    return decrypted_message
-
-if __name__ == "__main__":
-    original_message = "Hello, World! This is the August Cipher."
-    print("Original: ", original_message)
-
-    encrypted = august_cipher_encrypt(original_message)
-    print("Encrypted:", encrypted)
-
-    decrypted = august_cipher_decrypt(encrypted)
-    print("Decrypted:", decrypted)
+if __name__ == '__main__':
+    original_message = "HELLO WORLD 123"
+    
+    august_encrypted = xor_august_cipher(original_message)
+    august_decrypted = xor_august_cipher(august_encrypted)
+    
+    print("Original Message:  ", original_message)
+    print("August Encrypted:  ", august_encrypted)
+    print("August Decrypted:  ", august_decrypted)
